21541f52b67ff703510a5a1567c6059d3544a414
김재이 <jae2@kakao.com>
2023-03-29 14:50:15 +0900

    2222

---
+++ py/0329/ex1.py
@@ -0,0 +1,9 @@
+##input()과 int()활용
+
+name = input("이름이 뭔가요?")
+print("제 이름은", name, "입니다.")
+age = input("나이는?")
+print("나이는", age, "입니다.")
+print("아버지 나이는", int(age) + 30,"입니다")
+# 형변환 -> 자료형타입을 바꿔주기 int("30") str(30)
+
