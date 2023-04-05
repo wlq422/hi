dd363b102c4503bd85ec5a0aedbe34ecccc8dc5f
jae2 <jae2@kakao.com>
2023-04-05 16:21:11 +0900

    1qwq

---
+++ py/0405/5.py
@@ -0,0 +1,24 @@
+#sum, 0-10까지 숫자를 찍음
+# sum을 구할것임
+sum = 0
+n =0
+while n<11 :
+    sum =+ n
+    print(n, "  번째 sum : ", sum)
+    n = n +1
+
+print("while 밖에서 sum의 값 : " ,sum)
+n = n + 1
+
+print( "while 밖에서 sum의 값 : ",sum)
+
+while 0:
+    print("실행이 되지 않음")
+
+print("while 0 다음 줄입니다.")
+
+#while True :
+#    print ("무한루프")
+
+#while False:
+#   print("실행이 되지 않음")
