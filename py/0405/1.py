6ab002fc5adc7db308bebf8f175caefd420e1856
jae2 <jae2@kakao.com>
2023-04-05 14:19:33 +0900

    gdgd

---
+++ py/0405/1.py
@@ -0,0 +1,17 @@
+#0405
+#문자열
+
+str = "파이썬수업 씨수업 파이썬수업 파이썬수업"
+
+
+
+#replace
+print(str.replace("파이썬","자바",3))
+
+#count
+print(str.count("파이썬"))
+
+#find , index
+print("find : " , str.find("파이썬") , "index : " , str.index("씨"))
+print(str.find("AI"))
+print(str.index("AI"))
